571947e25f0783d1ebff6ee7a36229dd0e5c3419
clemchansf <clem.chan.sf@gmail.com>
2017-11-10 23:52:09 -0800

    add demo link to README

--- src/data.js
+++ src/data.js
@@ -1,21 +1,24 @@
 const data = [
   {
+    id: 1,
     name: "William",
     style: {backgroundColor: '#8e6e95'},
     children: ['child 1', 'child 2'],
-    color: ""
   },
   {
+    id: 2,
     name: "Connie",
     style: {backgroundColor: '#39a59c'},
     children: ['child 1', 'child 2'],
   },
   {
+    id: 3,
     name: "Billy",
     style: {backgroundColor: '#344759'},
     children: ['child 1', 'child 2'],
   },
   {
+    id: 4,
     name: "Sue",
     style: {backgroundColor: '#e8741e'},
     children: ['child 1', 'child 2'],
