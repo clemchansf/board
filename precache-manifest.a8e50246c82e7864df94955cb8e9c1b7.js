342e4dc3dd9002b7388a20f89f0ce7ac6331410c
Clem <clem.chan.sf@gmail.com>
2021-02-24 13:24:52 -0800

    Updates

---
+++ precache-manifest.a8e50246c82e7864df94955cb8e9c1b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "faa2360401b0b2258652",
+    "url": "/board/static/css/main.ff57248b.chunk.css"
+  },
+  {
+    "revision": "faa2360401b0b2258652",
+    "url": "/board/static/js/main.2642a23b.chunk.js"
+  },
+  {
+    "revision": "6a11764b31758448e182",
+    "url": "/board/static/js/runtime~main.05aaee5c.js"
+  },
+  {
+    "revision": "5ec576169542832e3806",
+    "url": "/board/static/js/2.d537253e.chunk.js"
+  },
+  {
+    "revision": "53b7193b84a336af22bc71774d4b3faa",
+    "url": "/board/index.html"
+  }
+];
